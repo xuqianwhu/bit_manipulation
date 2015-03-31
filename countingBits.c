033aec2cc99c60188fbd24ab4945be2afeca95dd
dc <xuqianwhu@gmail.com>
2015-03-31 16:36:34 +0800

    Add counting leading bit 0

--- countingBits.c
+++ countingBits.c
@@ -22,6 +22,29 @@ int countingBits1(int value)
     return tmp;
 }
 
+int countingLeadingBits0(int value)
+{
+     int n = 0;
+     unsigned int tmp = (unsigned int)value;
+     printf("\n");
+     printf("tmp %u ", tmp);
+     printValue(tmp, 32);
+     printf("\n\n");
+
+     if (tmp == 0)
+     {
+         return 32;
+     }
+
+     if(tmp <= 0x0000FFFF) {n = n + 16; tmp = tmp << 16;}
+     if(tmp <= 0x00FFFFFF) {n = n +  8; tmp = tmp <<  8;}
+     if(tmp <= 0x0FFFFFFF) {n = n +  4; tmp = tmp <<  4;}
+     if(tmp <= 0x3FFFFFFF) {n = n +  2; tmp = tmp <<  2;}
+     if(tmp <= 0x7FFFFFFF) {n = n +  1;}
+
+     return n;
+}
+
 
 int value = -3743;
 
@@ -29,6 +52,10 @@ int value = -3743;
 void main()
 {
     printf("There are %d bit 1 in value %#x %d ", countingBits1(value), value, value);
+    printValue(value, 32);
+    printf("\n");
+
+    printf("There are %d leading bits 0 in value %#x %d ", countingLeadingBits0(value), value, value);
     printValue(value, 32);
     printf("\n");
 }
